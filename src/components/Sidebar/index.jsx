6209997201275a30bd6b80a9ba49373d35bc527b
Brennon <brennonmeireles7@gmail.com>
2024-04-19 11:37:11 -0300

    fix: teste deploy

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,6 @@
 import { LiaHomeSolid } from "react-icons/lia";
 import { BsBox } from "react-icons/bs";
 
-
 import "./style.css"
 
 export default function Sidebar() {
@@ -11,7 +10,7 @@ export default function Sidebar() {
                 <ul className="sidebar-btn">
                     <li><LiaHomeSolid />Home</li>
                     <h4>INVENTORY</h4>
-                    <li><BsBox />Products</li>
+                    <li><BsBox />Link</li>
                     <li><BsBox />New Products</li>
                 </ul>
             </article>
