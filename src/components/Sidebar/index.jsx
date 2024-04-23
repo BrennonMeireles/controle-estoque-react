bd095abffd27df101308d23f255579572a34a597
Brennon <brennonmeireles7@gmail.com>
2024-04-23 10:24:23 -0300

    fix: remove old button

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -10,7 +10,6 @@ export default function Sidebar() {
                 <ul className="sidebar-btn">
                     <li><LiaHomeSolid /> <a href="/">Home</a></li>
                     <h4>INVENTORY</h4>
-                    <li><BsBox />Link</li>
                     <li><BsBox /><a href="/controle-estoque-react-omega.vercel.app/add-product">Add Product</a></li>
                 </ul>
             </article>
