36c921198dfbc419ed21cc79440f647d43f7ae96
gabriel <gabrieldosanjosdbz@gmil.com>
2024-04-24 10:50:00 -0300

    fix: adjust the creation component of a product

--- src/components/addNewProd/index.jsx
+++ src/components/addNewProd/index.jsx
@@ -1,70 +1,127 @@
-import "./style.css"
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./style.css";
+import { useNavigate } from 'react-router-dom';
 
 export default function AddNew() {
-    return(
-        <section className="section-addProd">
-                <article className="title">
-                    <div className="div-input">
-                        <label htmlFor="name">Name :</label>
-                        <input className="add-input" type="text" name="name" id="name" placeholder="Type name product"/>
-                    </div>
-                    <div className="div-input">
-                        <label className="sku-label" htmlFor="sku">SKU :</label>
-                        <input className="sku-input" type="text" name="sku" id="sku" placeholder="Type SKU product" />
-                    </div>
-                </article>  
-                <section>
-                    <article className="banner-stock">
-                        <div className="product-img">
-                            <img src="/src/assets/upload_icon.svg" alt="" />
-                            <input className="add-input-upload" type="text" name="img" id="name" placeholder="Paste here image URL" />
-                        </div>
-                        <div className="area-stock">
-                            <div className="bar-code">
-                                <img src="/src/assets/codigo-de-barras.svg" alt="codigo-de-barras" />
-                            </div>
-                            <div className="stock-card">
-                                <div className="title-stock">
-                                    <h2>Stock</h2>
-                                </div>
-                                <div className="data-stock">
-                                    <h3>QUANTITY AT HAND</h3>
-                                    <div className="div-stock-input">
-                                        <label htmlFor="estoque">Stock :</label>
-                                        <input className="add-input" type="text" name="estoque" id="estoque" placeholder="Type stock"/>
-                                    </div>
-                                    <button>Adjust Stock</button>
-                                    <h4>Status <span>low stock</span></h4>
-                                </div>
-                            </div>
-                            <div className="action-buttons">
-                                <button className="btn-delete">Delete Product</button>
-                                <button className="btn-add">Add Product</button>
-                            </div>
-                        </div>
-                    </article>
+    const navigate = useNavigate(); // Inicialize useHistory
 
-                    <article className="input-list">
-                        <ul className="list-product">
-                            <div className="div-input-list"> 
-                                <label htmlFor="custo">Purchase Coust:</label>
-                                <input className="add-input" type="text" name="custo" id="custo" placeholder="Type purchase coust" />
+    const [produto, setProduto] = useState({
+        nome: '',
+        sku: '',
+        descricao: '',
+        quantidade: '',
+        marca: '',
+        preco: '',
+        cor: '',
+        imagem: ''
+    });
+
+    // Função para lidar com mudanças nos campos de formulário
+    const handleChange = (e) => {
+        // Extrai o nome e o valor do campo de formulário que acionou o evento
+        const { name, value } = e.target;
+
+        // Atualiza o estado 'produto' com as novas informações do campo de formulário
+        // Clona o objeto 'produto' existente e atualiza a propriedade 'name' com o valor 'value
+        setProduto({ ...produto, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+            // Verifica se algum campo está vazio
+        if (!produto.nome || !produto.sku || !produto.descricao || !produto.quantidade || !produto.marca || !produto.preco || !produto.cor || !produto.imagem) {
+            // Encontra o primeiro campo vazio
+            const camposFaltando = Object.keys(produto).find(key => !produto[key]);
+            // Exibe um alerta informando ao usuário qual campo está faltando
+            alert(`Por favor, preencha o campo "${camposFaltando}"!`);
+            // Retorna imediatamente para interromper o envio do formulário
+        }
+        try {
+            const response = await axios.post('http://localhost:3000/', produto); // criando o post pelo axios
+            console.log('Produto adicionado:', response.data);
+            // Limpar os campos do formulário após o envio bem-sucedido, se necessário
+            setProduto({
+                nome: '',
+                sku: '',
+                descricao: '',
+                quantidade: '',
+                marca: '',
+                preco: '',
+                cor: '',
+                imagem: ''
+            });
+
+            // Redireciona para a página inicial após o envio bem-sucedido
+            navigate('/'); // Redireciona para a página inicial
+        } catch (error) {
+            alert('Erro ao adicionar produto:', error);
+        }
+    };
+
+    return (
+        <section className="section-addProd">
+            <article className="title">
+                <div className="div-input">
+                    <label htmlFor="name">Name :</label>
+                    <input className="add-input" type="text" name="nome" value={produto.nome} onChange={handleChange} placeholder="Type name product" required />
+                </div>
+                <div className="div-input">
+                    <label className="sku-label" htmlFor="sku">SKU :</label>
+                    <input className="sku-input" type="text" name="sku" value={produto.sku} onChange={handleChange} placeholder="Type SKU product" required />
+                </div>
+            </article>
+            <section>
+                <article className="banner-stock">
+                    <div className="product-img">
+                        <img src="/src/assets/upload_icon.svg" alt="" />
+                        <input className="add-input-upload" type="text" name="imagem" value={produto.imagem} onChange={handleChange} placeholder="Paste here image URL" required />
+                    </div>
+                    <div className="area-stock">
+                        <div className="bar-code">
+                            <img src="/src/assets/codigo-de-barras.svg" alt="codigo-de-barras" />
+                        </div>
+                        <div className="stock-card">
+                            <div className="title-stock">
+                                <h2>Stock</h2>
                             </div>
-                            <div className="div-input-list">
-                                <label htmlFor="color">Color :</label>
-                                <input className="add-input" type="text" name="color" id="color" placeholder="Type color product" />
+                            <div className="data-stock">
+                                <h3>QUANTITY AT HAND</h3>
+                                <div className="div-stock-input">
+                                    <label htmlFor="estoque">Stock :</label>
+                                    <input className="add-input" type="text" name="quantidade" value={produto.quantidade} onChange={handleChange} placeholder="Type stock" required />
+                                </div>
                             </div>
-                            <div className="div-input-list">
-                                <label htmlFor="marca">Brand :</label>
-                                <input className="add-input" type="text" name="marca" id="marca" placeholder="Type brand" />
-                            </div> 
-                            <div className="div-input-list">
-                                <label htmlFor="descricao">Description :</label>
-                                <input className="add-input" type="text" name="descricao" id="descricao" placeholder="Type description" />
-                            </div>
-                        </ul>
-                    </article>
-                </section>
+                        </div>
+                        <div className="action-buttons">
+                            {/* Adicionei o evento onClick para chamar a função handleSubmit quando o botão for clicado */}
+                            <button className="btn-add" onClick={handleSubmit}>Add Product</button>
+                        </div>
+                    </div>
+                </article>
+
+                <article className="input-list">
+                    <ul className="list-product">
+                        <div className="div-input-list">
+                            <label htmlFor="custo">Purchase Coust:</label>
+                            <input className="add-input" type="text" name="preco" value={produto.preco} onChange={handleChange} placeholder="Type purchase coust" required />
+                        </div>
+                        <div className="div-input-list">
+                            <label htmlFor="color">Color :</label>
+                            <input className="add-input" type="text" name="cor" value={produto.cor} onChange={handleChange} placeholder="Type color product" required />
+                        </div>
+                        <div className="div-input-list">
+                            <label htmlFor="marca">Brand :</label>
+                            <input className="add-input" type="text" name="marca" value={produto.marca} onChange={handleChange} placeholder="Type brand" required />
+                        </div>
+                        <div className="div-input-list">
+                            <label htmlFor="descricao">Description :</label>
+                            <input className="add-input" type="text" name="descricao" value={produto.descricao} onChange={handleChange} placeholder="Type description" required />
+                        </div>
+                    </ul>
+                </article>
+            </section>
         </section>
-    )
+    );
 }
