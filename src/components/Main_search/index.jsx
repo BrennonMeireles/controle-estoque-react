74aecad5e74476602623e39ab9b293f942f64cfa
Gabriel dos Anjos <gabrieldosanjosdbz@gmail.com>
2024-04-30 15:02:23 -0300

    fix: update index.jsx Main, viewProduct, Header, addNewProd and delete api.js

--- src/components/Main_search/index.jsx
+++ src/components/Main_search/index.jsx
@@ -2,6 +2,7 @@ import "./style.css"
 import axios from "axios"
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Main_search() {
     const {search} = useParams()
@@ -9,14 +10,14 @@ export default function Main_search() {
     const [produtos, setProdutos] = useState([]);
 
     useEffect(() => {
-        axios.get(`http://localhost:3000/search/${search}`)
+        axios.get(`https://api-estoque-rh46.onrender.com/search/${search}`)
         .then((res) => {
             setProdutos(res.data);
         })
         .catch(error => {
             console.error('Erro ao buscar produtos:', error);
         });
-    }, [produtos])
+    }, [search])
 
     return (
         <main>
@@ -26,31 +27,33 @@ export default function Main_search() {
                     <span> All Products</span>
                 </h1>
             </article>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Product Name</th>
-                        <th>SKU</th>
-                        <th>Brand</th>
-                        <th>Stock in Hand</th>
-                        <th>Reorder Level</th>
-                        <th>Image</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {produtos.map(produto => (
-                        <tr key={produto._id}>  
-                        <td><img className="img-preview" src={produto.imagem} alt="product image" /></td>
-                        <td><a href={`controle-estoque-react-omega.vercel.app/product/${produto._id}`}>{produto.nome}</a></td>
-                        <td>{produto.sku}</td>
-                        <td>{produto.marca}</td>
-                        <td>{produto.quantidade}</td>
-                        <td>10</td>
-                    </tr>
-                    ))}
-                </tbody>
-
-            </table>
+            {produtos.length === 0 && <h1>No products with name "{search}" found. Try again</h1>}
+            {produtos.length > 0 && 
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>SKU</th>
+                            <th>Brand</th>
+                            <th>Stock in Hand</th>
+                            <th>Reorder Level</th>
+                            <th>Image</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {produtos.map(produto => (
+                            <tr key={produto._id}>  
+                            <td><img className="img-preview" src={produto.imagem} alt="product image" /></td>
+                            <td><a href={`/controle-estoque-react-omega.vercel.app/product/${produto._id}`}>{produto.nome}</a></td>
+                            <td>{produto.sku}</td>
+                            <td>{produto.marca}</td>
+                            <td>{produto.quantidade}</td>
+                            <td>10</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            }
         </main>
     )
 }
