dfb798c80527a351bf490f7b13888ee9d409004e
gabriel <gabrieldosanjosdbz@gmil.com>
2024-05-09 09:15:06 -0300

    create loading

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -4,16 +4,21 @@ import { useState, useEffect } from "react";
 
 export default function Main() {
     const [produtos, setProdutos] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true);
+
         axios.get('https://api-estoque-rh46.onrender.com/')
         .then((res) => {
+            setLoading(false)
             setProdutos(res.data);
         })
         .catch(error => {
             console.error('Erro ao buscar produtos:', error);
         });
-    }, [produtos])
+
+    }, [])
 
     return (
         <main>
@@ -23,31 +28,33 @@ export default function Main() {
                     <span> All Products</span>
                 </h1>
             </article>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Product Name</th>
-                        <th>SKU</th>
-                        <th>Brand</th>
-                        <th>Stock in Hand</th>
-                        <th>Reorder Level</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {produtos.map(produto => (
-                        <tr key={produto._id}>  
-                        <td><img className="img-preview" src={produto.imagem} alt="product image" /></td>
-                        <td><a href={`/product/${produto._id}`}>{produto.nome}</a></td>
-                        <td>{produto.sku}</td>
-                        <td>{produto.marca}</td>
-                        <td>{produto.quantidade}</td>
-                        <td>10</td>
-                    </tr>
-                    ))}
-                </tbody>
-
-            </table>
+            {loading && <h1>Loading...</h1>}
+                {!loading && 
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Product Name</th>
+                            <th>SKU</th>
+                            <th>Brand</th>
+                            <th>Stock in Hand</th>
+                            <th>Reorder Level</th>
+                        </tr>
+                    </thead>
+                    <tbody> 
+                        {produtos.map(produto => (
+                            <tr key={produto._id}>  
+                            <td><img className="img-preview" src={produto.imagem} alt="product image" /></td>
+                            <td><a href={`/product/${produto._id}`}>{produto.nome}</a></td>
+                            <td>{produto.sku}</td>
+                            <td>{produto.marca}</td>
+                            <td>{produto.quantidade}</td>
+                            <td>10</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            }
         </main>
     )
 }
--- src/components/Main_search/index.jsx
+++ src/components/Main_search/index.jsx
@@ -1,19 +1,22 @@
-
-import "./style.css"
-import axios from "axios"
+import "./style.css";
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function Main_search() {
-    const {search} = useParams()
+    const { search } = useParams()
 
     const [produtos, setProdutos] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
+
         axios.get(`https://api-estoque-rh46.onrender.com/search/${search}`)
         .then((res) => {
             setProdutos(res.data);
+            setLoading(false)
         })
         .catch(error => {
             console.error('Erro ao buscar produtos:', error);
@@ -28,33 +31,36 @@ export default function Main_search() {
                     <span> All Products</span>
                 </h1>
             </article>
-            {produtos.length === 0 && <h1>No products with name "{search}" found. Try again</h1>}
-            {produtos.length > 0 && 
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Product Name</th>
-                            <th>SKU</th>
-                            <th>Brand</th>
-                            <th>Stock in Hand</th>
-                            <th>Reorder Level</th>
-                            <th>Image</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {produtos.map(produto => (
-                            <tr key={produto._id}>  
-                            <td><img className="img-preview" src={produto.imagem} alt="product image" /></td>
-                            <td><a href={`/product/${produto._id}`}>{produto.nome}</a></td>
-                            <td>{produto.sku}</td>
-                            <td>{produto.marca}</td>
-                            <td>{produto.quantidade}</td>
-                            <td>10</td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
-            }
+            {loading ? null : (
+                produtos.length === 0 ? (
+                    <h1>No products with name "{search}" found. Try again</h1>
+                ) : (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Product Name</th>
+                                <th>SKU</th>
+                                <th>Brand</th>
+                                <th>Stock in Hand</th>
+                                <th>Reorder Level</th>
+                                <th>Image</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {produtos.map(produto => (
+                                <tr key={produto._id}>  
+                                    <td><img className="img-preview" src={produto.imagem} alt="product image" /></td>
+                                    <td><a href={`/product/${produto._id}`}>{produto.nome}</a></td>
+                                    <td>{produto.sku}</td>
+                                    <td>{produto.marca}</td>
+                                    <td>{produto.quantidade}</td>
+                                    <td>10</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )
+            )}
         </main>
     )
 }
